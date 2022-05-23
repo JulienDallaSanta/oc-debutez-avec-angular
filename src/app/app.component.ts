5ed82dccf95c2e914de89c2be5e7cb4fcbb4f30e
Julien Dalla Santa <j.dallasanta@defi-informatique.fr>
2022-05-23 17:36:28 +0200

    modify snaps number

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -15,21 +15,21 @@ export class AppComponent implements  OnInit{
         title: 'Archibald',
         description: 'Mon meilleur ami depuis tout petit !',
         createdDate: new Date(),
-        snaps: 0,
+        snaps: 30,
         imageUrl: 'https://cdn.pixabay.com/photo/2015/05/31/16/03/teddy-bear-792273_1280.jpg'
       },
       {
         title: 'Three Rock Mountain',
         description: 'Un endroit magnifique pour les randonnées.',
         createdDate: new Date(),
-        snaps: 0,
+        snaps: 85,
         imageUrl: 'https://cdn.pixabay.com/photo/2015/05/31/16/03/teddy-bear-792273_1280.jpg'
       },
       {
         title: 'Un bon repas',
         description: 'Mmmh que c\'est bon !',
         createdDate: new Date(),
-        snaps: 0,
+        snaps: 155,
         imageUrl: 'https://wtop.com/wp-content/uploads/2020/06/HEALTHYFRESH.jpg',
         location: 'Abos'
       }
