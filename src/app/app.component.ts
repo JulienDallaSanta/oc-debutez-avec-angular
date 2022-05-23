8bd4bdbb1dfcc80c8a3db05c76632ee9c1f6a313
Julien Dalla Santa <j.dallasanta@defi-informatique.fr>
2022-05-23 17:06:40 +0200

    first commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,38 @@
-import { Component } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
+import {FaceSnap} from "./models/face-snap.model";
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
-export class AppComponent {
-  title = 'snapface';
+export class AppComponent implements  OnInit{
+  faceSnaps!: FaceSnap[];
+
+  ngOnInit(): void {
+    this.faceSnaps = [
+      {
+        title: 'Archibald',
+        description: 'Mon meilleur ami depuis tout petit !',
+        createdDate: new Date(),
+        snaps: 0,
+        imageUrl: 'https://cdn.pixabay.com/photo/2015/05/31/16/03/teddy-bear-792273_1280.jpg'
+      },
+      {
+        title: 'Three Rock Mountain',
+        description: 'Un endroit magnifique pour les randonnées.',
+        createdDate: new Date(),
+        snaps: 0,
+        imageUrl: 'https://cdn.pixabay.com/photo/2015/05/31/16/03/teddy-bear-792273_1280.jpg'
+      },
+      {
+        title: 'Un bon repas',
+        description: 'Mmmh que c\'est bon !',
+        createdDate: new Date(),
+        snaps: 0,
+        imageUrl: 'https://wtop.com/wp-content/uploads/2020/06/HEALTHYFRESH.jpg',
+        location: 'Abos'
+      }
+    ]
+  };
 }
---
+++ src/app/models/face-snap.model.ts
@@ -0,0 +1,9 @@
+export class FaceSnap{
+  public title!: string;
+  public description!: string;
+  public createdDate!: Date;
+  public snaps!: number;
+  public imageUrl!: string;
+  public location?: string;
+
+}
